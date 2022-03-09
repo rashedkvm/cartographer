f5b850820ae344f5ae7a7522a238fdd8e575025b
Sam Coward <scoward@vmware.com>
2022-03-09 14:29:33 -0500

    Moving. again. and now tests

---
+++ pkg/selector/matcher_test.go
@@ -0,0 +1,380 @@
+package selector_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+	"github.com/vmware-tanzu/cartographer/pkg/selector"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	labels2 "k8s.io/apimachinery/pkg/labels"
+
+	"github.com/vmware-tanzu/cartographer/pkg/apis/v1alpha1"
+)
+
+var _ = Describe("BestSelectorMatchIndices", func() {
+
+	type testcase struct {
+		selectable               selector.Selectable
+		selectors                selectingObjectList
+		exptectedSelectorIndices []int
+	}
+
+	DescribeTable(
+		"cases",
+		func(tc testcase) {
+			actual, _ := selector.BestSelectorMatchIndices(
+				tc.selectable, tc.selectors,
+			)
+
+			if tc.exptectedSelectorIndices == nil {
+				Expect(actual).To(BeNil())
+				return
+			}
+
+			Expect(actual).To(Equal(tc.exptectedSelectorIndices))
+		},
+
+		// ---------- Label Selectors
+
+		Entry("empty selectors", testcase{
+			selectable: selectable{
+				labels: labels2.Set{}},
+			exptectedSelectorIndices: nil,
+		}),
+
+		Entry("complete mismatched selectors & selectors", testcase{
+			selectable: selectable{
+				labels: labels2.Set{
+					"type": "web",
+				},
+			},
+			selectors: selectingObjectList{
+				newSelectingObject(
+					labels2.Set{
+						"my": "label",
+					},
+					nil,
+					fields{
+						v1alpha1.FieldSelectorRequirement{
+							Key:      "Spec.libertyGibbet",
+							Operator: "Exists",
+							Values:   nil,
+						},
+					},
+				),
+			},
+			exptectedSelectorIndices: nil,
+		}),
+
+		Entry("partial match; selectors with less labels than selectors", testcase{
+			selectable: selectable{
+				labels: labels2.Set{
+					"type": "web",
+					"test": "tekton",
+				}},
+			selectors: selectingObjectList{
+				newSelectingObject(
+					labels2.Set{
+						"type": "web",
+					},
+					nil,
+					nil,
+				),
+			},
+			exptectedSelectorIndices: []int{0},
+		}),
+
+		Entry("partial match; selectors with less labels than target", testcase{
+			selectable: selectable{
+				labels: labels2.Set{
+					"type": "web",
+				}},
+			selectors: selectingObjectList{
+				newSelectingObject(
+					labels2.Set{
+						"type": "web",
+						"test": "tekton",
+					},
+					nil,
+					nil,
+				),
+			},
+			exptectedSelectorIndices: nil,
+		}),
+
+		Entry("absolute match", testcase{
+			selectable: selectable{
+				labels: labels2.Set{
+					"type": "web",
+					"test": "tekton",
+				}},
+			selectors: selectingObjectList{
+				newSelectingObject(
+					labels2.Set{
+						"type": "web",
+						"test": "webvalue",
+					},
+					nil,
+					nil,
+				),
+				newSelectingObject(
+					labels2.Set{
+						"type": "web",
+						"test": "tekton",
+					},
+					nil,
+					nil,
+				),
+				newSelectingObject(
+					labels2.Set{
+						"type": "mobile",
+						"test": "mobilevalue",
+					},
+					nil,
+					nil,
+				),
+			},
+			exptectedSelectorIndices: []int{1},
+		}),
+
+		Entry("exact partial match", testcase{
+			selectable: selectable{
+				labels: labels2.Set{
+					"type":  "web",
+					"test":  "tekton",
+					"scan":  "security",
+					"input": "image",
+				}},
+			selectors: selectingObjectList{
+				newSelectingObject(
+					labels2.Set{
+						"type": "atype",
+						"test": "tekton",
+						"scan": "ascan",
+					},
+					nil,
+					nil,
+				),
+				newSelectingObject(
+					labels2.Set{
+						"type": "web",
+						"test": "tekton",
+						"scan": "security",
+					},
+					nil,
+					nil,
+				),
+				newSelectingObject(
+					labels2.Set{
+						"type":  "web",
+						"test":  "tekton",
+						"input": "image",
+					},
+					nil,
+					nil,
+				),
+			},
+			exptectedSelectorIndices: []int{1,2},
+		}),
+
+		Entry("exact match with no extras", testcase{
+			selectable: selectable{
+				labels: labels2.Set{
+					"type": "web",
+					"test": "tekton",
+					"scan": "security",
+				}},
+			selectors: selectingObjectList{
+				newSelectingObject(
+					labels2.Set{
+						"type": "atype",
+						"test": "tekton",
+						"scan": "ascan",
+					},
+					nil,
+					nil,
+				),
+				newSelectingObject(
+					labels2.Set{
+						"type": "web",
+						"test": "tekton",
+						"scan": "security",
+					},
+					nil,
+					nil,
+				),
+				newSelectingObject(
+					labels2.Set{
+						"type":  "web",
+						"test":  "tekton",
+						"scan":  "security",
+						"input": "image",
+					},
+					nil,
+					nil,
+				),
+			},
+			exptectedSelectorIndices: []int{1},
+		}),
+
+		Entry("match selectors with many fields in selectors", testcase{
+			selectable: selectable{
+				Spec: Spec{
+					Color: "red",
+					Age:   4,
+				},
+			},
+			selectors: selectingObjectList{
+				newSelectingObject(
+					nil,
+					nil,
+					fields{
+						{
+							Key:      "Spec.Color",
+							Operator: "NotIn",
+							Values:   []string{"green", "blue"},
+						},
+					},
+				),
+			},
+			exptectedSelectorIndices: []int{0},
+		}),
+	)
+
+	Describe("malformed selectors", func() {
+		Context("label selector invalid", func() {
+			var sel selectingObjectList
+			BeforeEach(func() {
+				sel = selectingObjectList{
+					newSelectingObjectWithID(
+						"valid-selector",
+						"Special",
+						labels2.Set{
+							"fred": "derf",
+						},
+						nil,
+						nil,
+					),
+					newSelectingObjectWithID(
+						"invalid-selector",
+						"Special",
+						labels2.Set{
+							"fred-": "derf-",
+						},
+						nil,
+						nil,
+					),
+				}
+			})
+
+			It("returns an error", func() {
+				_, err := selector.BestSelectorMatchIndices(selectable{}, sel)
+				Expect(err).To(MatchError(ContainSubstring("selector matchLabels or matchExpressions are not valid")))
+				Expect(err).To(MatchError(ContainSubstring("key: Invalid value")))
+				Expect(err.GetSelectingObjectIndex()).To(Equal(1))
+			})
+		})
+
+		Context("expression selector invalid", func() {
+			var sel selectingObjectList
+			BeforeEach(func() {
+				sel = selectingObjectList{
+					newSelectingObjectWithID(
+						"valid-selector",
+						"Special",
+						nil,
+						[]metav1.LabelSelectorRequirement{
+							{
+								Key:      "valid",
+								Operator: "Exists",
+								Values:   nil,
+							},
+						},
+						nil,
+					),
+					newSelectingObjectWithID(
+						"invalid-selector",
+						"Special",
+						nil,
+						[]metav1.LabelSelectorRequirement{
+							{
+								Key:      "fred",
+								Operator: "Matchingest",
+								Values:   nil,
+							},
+						},
+						nil,
+					),
+				}
+			})
+
+			It("returns an error", func() {
+				_, err := selector.BestSelectorMatchIndices(selectable{}, sel)
+				Expect(err).To(MatchError(ContainSubstring("selector matchLabels or matchExpressions are not valid")))
+				// TODO: 'pod' - Hmmmmm - perhaps we shouldn't be using v1 code?
+				Expect(err).To(MatchError(ContainSubstring("\"Matchingest\" is not a valid pod selector operator")))
+				Expect(err.GetSelectingObjectIndex()).To(Equal(1))
+			})
+		})
+	})
+})
+
+type fields []v1alpha1.FieldSelectorRequirement
+
+type Spec struct {
+	Color string `json:"color"`
+	Age   int    `json:"age"`
+}
+
+type selectable struct {
+	labels map[string]string
+	Spec   `json:"spec"`
+}
+
+func (o selectable) GetLabels() map[string]string {
+	return o.labels
+}
+
+type selectingObject struct {
+	metav1.TypeMeta
+	metav1.ObjectMeta
+	v1alpha1.Selector
+}
+
+func newSelectingObject(labels labels2.Set, expressions []metav1.LabelSelectorRequirement, fields []v1alpha1.FieldSelectorRequirement) *selectingObject {
+	return newSelectingObjectWithID("testSelectingObject", "Test", labels, expressions, fields)
+}
+
+func newSelectingObjectWithID(name, kind string, labels labels2.Set, expressions []metav1.LabelSelectorRequirement, fields []v1alpha1.FieldSelectorRequirement) *selectingObject {
+	return &selectingObject{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       kind,
+			APIVersion: "testv1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Selector: v1alpha1.Selector{
+			LabelSelector: metav1.LabelSelector{
+				MatchLabels:      labels,
+				MatchExpressions: expressions,
+			},
+			MatchFields: fields,
+		},
+	}
+}
+
+func (b *selectingObject) GetSelector() v1alpha1.Selector {
+	return b.Selector
+}
+
+type selectingObjectList []*selectingObject
+
+func (l selectingObjectList) EachSelectingObject(handler func(idx int, selectingObject selector.SelectingObject) selector.SelectorMatchError) selector.SelectorMatchError {
+	for idx, item := range l {
+		if err := handler(idx, item); err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- pkg/selector/selector.go
+++ pkg/selector/selector.go
@@ -16,38 +16,11 @@ package selector
 
 import (
 	"fmt"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 
 	"github.com/vmware-tanzu/cartographer/pkg/apis/v1alpha1"
 	"github.com/vmware-tanzu/cartographer/pkg/eval"
 )
 
-type Selectable interface {
-	GetLabels() map[string]string
-}
-
-type SelectingObject interface {
-	GetSelector() v1alpha1.Selector
-}
-
-type SelectorMatchError interface {
-	error
-	GetSelectingObjectIndex() int
-}
-
-type selectorMatchError struct {
-	Err                  error
-	SelectingObjectIndex int
-}
-
-func (e selectorMatchError) Error() string {
-	return e.Err.Error()
-}
-
-func (e selectorMatchError) GetSelectingObjectIndex() int {
-	return e.SelectingObjectIndex
-}
 
 func Matches(req v1alpha1.FieldSelectorRequirement, context interface{}) (bool, error) {
 	evaluator := eval.EvaluatorBuilder()
@@ -78,89 +51,4 @@ func Matches(req v1alpha1.FieldSelectorRequirement, context interface{}) (bool, 
 	default:
 		return false, fmt.Errorf("invalid operator %s for field selector", req.Operator)
 	}
-}
-
-type EnumerableSelectingObjects interface {
-	EachSelectingObject(handler func(index int, selectingObject SelectingObject) SelectorMatchError) SelectorMatchError
-}
-
-func BestSelectorMatchIndices(selectable Selectable, selectingObjects EnumerableSelectingObjects) ([]int, SelectorMatchError) {
-
-	//	if len(selectingObjects) == 0 { //FIXME: is this behavior still preserved?
-	//		return nil, nil, nil
-	//	}
-
-	var matchingSelectorIndices = map[int][]int{}
-	var highWaterMark = 0
-
-	err := selectingObjects.EachSelectingObject(func(idx int, selectingObject SelectingObject) SelectorMatchError {
-		selectors := selectingObject.GetSelector()
-
-		matchScore := 0
-		labelSelector := &metav1.LabelSelector{
-			MatchLabels:      selectors.MatchLabels,
-			MatchExpressions: selectors.MatchExpressions,
-		}
-
-		// -- Labels
-		sel, err := metav1.LabelSelectorAsSelector(labelSelector)
-		if err != nil {
-			return selectorMatchError{
-				Err:                  fmt.Errorf("selector matchLabels or matchExpressions are not valid: %w", err),
-				SelectingObjectIndex: idx,
-			}
-		}
-		if !sel.Matches(labels.Set(selectable.GetLabels())) {
-			return nil // Bail early!
-		}
-
-		matchScore += len(labelSelector.MatchLabels)
-		matchScore += len(labelSelector.MatchExpressions)
-
-		// -- Fields
-		allFieldsMatched, err := matchesAllFields(selectable, selectors.MatchFields)
-		if err != nil {
-			// Todo: test in unit test
-			return selectorMatchError{
-				Err:                  fmt.Errorf("failed to evaluate selector matchFields: %w", err),
-				SelectingObjectIndex: idx,
-			}
-		}
-		if !allFieldsMatched {
-			return nil // Bail early!
-		}
-		matchScore += len(selectors.MatchFields)
-
-		// -- decision time
-		if matchScore > 0 {
-			if matchingSelectorIndices[matchScore] == nil { //FIXME: needed?
-				matchingSelectorIndices[matchScore] = []int{}
-			}
-			if matchScore > highWaterMark {
-				highWaterMark = matchScore
-			}
-			matchingSelectorIndices[matchScore] = append(matchingSelectorIndices[matchScore], idx)
-		}
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return matchingSelectorIndices[highWaterMark], nil
-}
-
-func matchesAllFields(source interface{}, requirements []v1alpha1.FieldSelectorRequirement) (bool, error) {
-	for _, requirement := range requirements {
-		match, err := Matches(requirement, source)
-		//TODO: what happens if its JsonPathDoesNotExistError?
-		if err != nil {
-			return false, fmt.Errorf("unable to match field requirement with key [%s] operator [%s] values [%v]: %w", requirement.Key, requirement.Operator, requirement.Values, err)
-		}
-		if !match {
-			return false, nil
-		}
-	}
-	return true, nil
 }
